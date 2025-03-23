c6abe79336df12b825b92407e9248ed7fb259933
Abhijeet Kumar <abhijeetkumar2706@gmail.com>
2025-03-23 22:00:55 +0530

    initial chane

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -13,7 +13,7 @@ export async function POST(req) {
     const svixHeaders = {
         "svix-id": headerPayload.get("svix-id"),
         "svix-timestamp": headerPayload.get("svix-timestamp"),
-        "svix-signature": headerPayload.get("svix-signature"),
+        "svix-signature": headerPayload.get("svix-signature")
     };
 
     //get the payload and verrify it
